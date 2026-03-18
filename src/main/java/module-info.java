3ab573840c374925423af0a96c843402804c7e6f
Pablo C. <cortezrodriguezpablo@gmail.com>
2026-03-18 16:29:33 -0600

    Create initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.starsolutions.graphicsproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.starsolutions.graphicsproject to javafx.fxml;
+    exports com.starsolutions.graphicsproject;
+}
